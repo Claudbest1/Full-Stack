2cd382a011600a8e0a3c24103bc9b10b18cd2f4b
Claudbest1 <102980679+Claudbest1@users.noreply.github.com>
2022-05-27 10:16:28 +0100

    Add files via upload

---
+++ Reading and Counting text.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 16:33:33 2022
+
+@author: Claudius Odeyemi
+"""
+'''This code was written with Spyder IDE'''
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+
+def read_file_content(filename):
+    with open(filename) as f:
+        text=f.read()
+    return text
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    print(text)
+    #translate words by removing special characters
+    translate = text.maketrans({char: None for char in "'.,:*!"})
+    #split clean words
+    cleaned_words = text.lower().translate(translate).split()
+    #Create a dictionary for our word distribution
+    word_counter = {}
+    for word in cleaned_words:
+        if word in word_counter:
+            word_counter[word] += 1
+        else:
+            word_counter[word] = 1
+    return word_counter
+
+count_words()
